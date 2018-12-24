84e6d7ed48d33fa4a28d14f14becac1cf4830c26
Onyekachi Anyanwu <onyekachi@itbps.com>
2018-12-23 19:32:02 -0500

    Added more info

--- Serverside/src/com/itbps/fuelmgt/Employee.java
+++ Serverside/src/com/itbps/fuelmgt/Employee.java
@@ -1,6 +1,52 @@
 package com.itbps.fuelmgt;
 
-public class Employee 
-{
+import java.util.Date;
+
+public class Employee {
+	private String name;
+	private String firstName;
+	private String lastName;
+	private Date dateofhire;
+	private String ssn;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getDateofhire() {
+		return dateofhire;
+	}
+
+	public void setDateofhire(Date dateofhire) {
+		this.dateofhire = dateofhire;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
 
 }
